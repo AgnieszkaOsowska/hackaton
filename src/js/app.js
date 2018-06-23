ef649f3bdb30bd4353f39e0b6f150e1d1477a222
Unknown <comanderrr@gmail.com>
2018-06-24 01:35:37 +0200

    feat: json

--- src/js/app.js
+++ src/js/app.js
@@ -25,8 +25,21 @@ xhr.addEventListener('load', function () {
             var array = arr.slice(0, -1);
         }
         console.log(array);
-        // document.getElementById('output').innerHTML = arr;
+         document.getElementById('img1').src = (array[0].image.x1);
+         document.getElementById('img2').src = (array[1].image.x1);
+         document.getElementById('img3').src = (array[2].image.x1);
 
+         document.getElementById('title1').innerHTML = (array[0].title);
+         document.getElementById('title2').innerHTML = (array[1].title);
+         document.getElementById('title3').innerHTML = (array[2].title);
+
+         document.getElementById('description1').innerHTML = (array[0].description);
+         document.getElementById('description2').innerHTML = (array[1].description);
+         document.getElementById('description3').innerHTML = (array[2].description);
+
+         document.getElementById('author1').innerHTML = (`Author: ${array[0].author}`);
+         document.getElementById('author2').innerHTML = (`Author: ${array[1].author}`);
+         document.getElementById('author3').innerHTML = (`Author: ${array[2].author}`);
     } else {
         console.log(`Status ${this.status}`);
     }
